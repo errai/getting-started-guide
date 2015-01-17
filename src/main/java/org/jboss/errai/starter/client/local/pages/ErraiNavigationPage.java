a860989d0b249881f750ea40fbcdd2304ed404be
divd <divya.dadlani@gmail.com>
2015-01-17 17:19:03 -0500

    Added Errai Navigation Page

---
+++ src/main/java/org/jboss/errai/starter/client/local/pages/ErraiNavigationPage.java
@@ -0,0 +1,26 @@
+package org.jboss.errai.starter.client.local.pages;
+
+import com.google.gwt.dom.client.Element;
+import com.google.gwt.user.client.ui.Composite;
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.PageShowing;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+/**
+ * Created by Divya Dadlani <ddadlani@redhat.com>
+ */
+@Page
+@Templated
+public class ErraiNavigationPage extends Composite{
+    @PageShowing
+    public void onPageLoad() {
+        colorPage(this.getElement());
+    }
+
+    private static native void colorPage(Element e) /*-{
+
+        $wnd.Rainbow.color(e, function() {
+            console.log("Colored code elements");
+        });
+    }-*/;
+}
