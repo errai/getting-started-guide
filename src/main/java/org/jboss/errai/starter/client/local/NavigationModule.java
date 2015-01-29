2bec8078e5875a30694f77dbf6bb7e8bc93ac61a
divd <divya.dadlani@gmail.com>
2015-01-29 16:58:00 -0500

    Added a UniquePageRole called NavigationModule for ErraiNavigationPage.java to include it in the Navigation demo.

---
+++ src/main/java/org/jboss/errai/starter/client/local/NavigationModule.java
@@ -0,0 +1,9 @@
+package org.jboss.errai.starter.client.local;
+
+import org.jboss.errai.ui.nav.client.local.UniquePageRole;
+
+/**
+ * @author Divya Dadlani <ddadlani@redhat.com>
+ */
+public class NavigationModule implements UniquePageRole {
+}
--- src/main/java/org/jboss/errai/starter/client/local/pages/ErraiNavigationPage.java
+++ src/main/java/org/jboss/errai/starter/client/local/pages/ErraiNavigationPage.java
@@ -1,17 +1,30 @@
 package org.jboss.errai.starter.client.local.pages;
 
 import com.google.gwt.dom.client.Element;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.Window;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import org.jboss.errai.bus.client.ErraiBus;
+import org.jboss.errai.bus.client.api.base.MessageBuilder;
+import org.jboss.errai.bus.client.api.messaging.Message;
+import org.jboss.errai.bus.client.api.messaging.MessageCallback;
+import org.jboss.errai.starter.client.local.NavigationModule;
 import org.jboss.errai.ui.nav.client.local.Page;
 import org.jboss.errai.ui.nav.client.local.PageShowing;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import javax.inject.Inject;
 
 /**
  * Created by Divya Dadlani <ddadlani@redhat.com>
  */
-@Page
+@Page(role = NavigationModule.class)
 @Templated
 public class ErraiNavigationPage extends Composite{
+
     @PageShowing
     public void onPageLoad() {
         colorPage(this.getElement());
