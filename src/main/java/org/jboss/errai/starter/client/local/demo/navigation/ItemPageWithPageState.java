0ec84ee4a67145e292d3db446d55766f793c84cb
divd <divya.dadlani@gmail.com>
2015-01-30 17:32:55 -0500

    Mostly done with navigation examples, except for one HistoryToken example.

--- src/main/java/org/jboss/errai/starter/client/local/demo/navigation/ItemPageWithPageState.java
+++ src/main/java/org/jboss/errai/starter/client/local/demo/navigation/ItemPageWithPageState.java
@@ -1,9 +1,19 @@
 package org.jboss.errai.starter.client.local.demo.navigation;
 
+import com.google.common.collect.Multimap;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.Label;
 import com.google.gwt.user.client.ui.VerticalPanel;
-import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.starter.client.local.NavigationModule;
+import org.jboss.errai.ui.nav.client.local.*;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import javax.inject.Inject;
+import java.util.Collection;
 
 /**
  * @author Divya Dadlani <ddadlani@redhat.com>
@@ -11,4 +21,28 @@ import org.jboss.errai.ui.shared.api.annotations.Templated;
 @Page
 @Templated
 public class ItemPageWithPageState extends Composite {
+
+		@Inject
+		@DataField
+		Label printItemId;
+
+		@PageState
+		int itemId;
+
+		@Inject
+		@DataField
+		Button backToNavButton;
+
+		@Inject
+		TransitionToRole<NavigationModule> backToNav;
+
+		@PageShowing
+		public void onPageLoad() {
+				printItemId.setText("ItemID = " + itemId);
+		}
+
+		@EventHandler("backToNavButton")
+		public void onBackButtonClicked(ClickEvent e) {
+			backToNav.go();
+		}
 }
--- src/main/java/org/jboss/errai/starter/client/local/demo/navigation/WelcomePageWithPageState.java
+++ src/main/java/org/jboss/errai/starter/client/local/demo/navigation/WelcomePageWithPageState.java
@@ -1,13 +1,48 @@
 package org.jboss.errai.starter.client.local.demo.navigation;
 
+import com.google.common.collect.ImmutableMultimap;
+import com.google.common.collect.ImmutableMultimap.Builder;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.TransitionTo;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+
+import javax.inject.Inject;
 
 /**
  * @author Divya Dadlani <ddadlani@redhat.com>
  */
-@Page
-@Templated
-public class WelcomePageWithPageState extends Composite {
+@Page @Templated public class WelcomePageWithPageState extends Composite {
+
+		@Inject
+		@DataField
+		Button itemId2;
+
+		@Inject
+		@DataField
+		Button itemId4;
+
+		@Inject
+
+		TransitionTo<ItemPageWithPageState> goToItemPage;
+
+		@EventHandler("itemId2")
+		public void itemIdEquals2(ClickEvent e) {
+				Builder<String, String> mapBuilder = ImmutableMultimap.builder();
+				mapBuilder.put("itemId", "2");
+
+				goToItemPage.go(mapBuilder.build());
+		}
+
+		@EventHandler("itemId4")
+		public void itemIdEquals4(ClickEvent e) {
+				Builder<String, String> mapBuilder = ImmutableMultimap.builder();
+				mapBuilder.put("itemId", "4");
+
+				goToItemPage.go(mapBuilder.build());
+		}
 }
