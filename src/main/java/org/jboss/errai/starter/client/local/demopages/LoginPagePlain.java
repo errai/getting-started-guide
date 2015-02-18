d6e53c6abb7e19e45eddd7b7bdb4d1acd97d80f5
divd <divya.dadlani@gmail.com>
2015-02-18 18:10:55 -0500

    Added first Errai UI demo

---
+++ src/main/java/org/jboss/errai/starter/client/local/demopages/LoginPagePlain.java
@@ -0,0 +1,14 @@
+package org.jboss.errai.starter.client.local.demopages;
+
+import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.shared.api.annotations.Templated;
+import com.google.gwt.user.client.ui.Composite;
+
+/**
+ * @author Divya Dadlani <ddadlani@redhat.com>
+ */
+@Page
+@Templated
+public class LoginPagePlain extends Composite {
+
+}
--- src/main/java/org/jboss/errai/starter/client/local/pages/ErraiUIPage.java
+++ src/main/java/org/jboss/errai/starter/client/local/pages/ErraiUIPage.java
@@ -1,7 +1,14 @@
 package org.jboss.errai.starter.client.local.pages;
 
+import javax.inject.Inject;
+import org.jboss.errai.starter.client.local.demopages.LoginPagePlain;
 import org.jboss.errai.ui.nav.client.local.Page;
+import org.jboss.errai.ui.nav.client.local.TransitionAnchor;
+import org.jboss.errai.ui.shared.api.annotations.DataField;
+import org.jboss.errai.ui.shared.api.annotations.EventHandler;
 import org.jboss.errai.ui.shared.api.annotations.Templated;
+import com.google.gwt.event.dom.client.ClickEvent;
+import com.google.gwt.user.client.ui.Button;
 
 /**
  * @author Divya Dadlani <ddadlani@redhat.com>
@@ -9,6 +16,9 @@ import org.jboss.errai.ui.shared.api.annotations.Templated;
 @Page
 @Templated
 public class ErraiUIPage extends AbstractErraiModulePage {
-  
+
+  @Inject
+  @DataField
+  TransitionAnchor<LoginPagePlain> loginPagePlainAnchor;
 
 }
