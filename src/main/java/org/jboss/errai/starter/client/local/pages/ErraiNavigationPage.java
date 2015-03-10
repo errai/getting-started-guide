5474bc5684c96ab398c223a8c6b3dbe1ba34e006
divd <divya.dadlani@gmail.com>
2015-03-10 18:02:50 -0400

    Updated Navigation demos and code snippets

--- src/main/java/org/jboss/errai/starter/client/local/pages/ErraiNavigationPage.java
+++ src/main/java/org/jboss/errai/starter/client/local/pages/ErraiNavigationPage.java
@@ -2,6 +2,8 @@ package org.jboss.errai.starter.client.local.pages;
 
 import javax.inject.Inject;
 import org.jboss.errai.starter.client.local.NavigationModule;
+import org.jboss.errai.starter.client.local.demopages.LoginFormPage;
+import org.jboss.errai.starter.client.local.demopages.SignUpPageSubmit;
 import org.jboss.errai.starter.client.local.demopages.WelcomePageWithPageState;
 import org.jboss.errai.starter.client.local.demopages.WelcomePageWithTransition;
 import org.jboss.errai.ui.nav.client.local.Page;
@@ -20,10 +22,10 @@ import com.google.gwt.user.client.ui.Button;
 public class ErraiNavigationPage extends AbstractErraiModulePage {
 
     @Inject
-    TransitionTo<WelcomePageWithTransition> transitionDemo;
+    TransitionTo<LoginFormPage> transitionDemo;
 
     @Inject
-    TransitionTo<WelcomePageWithPageState> pageStateDemo;
+    TransitionTo<SignUpPageSubmit> pageStateDemo;
 
     @Inject
     @DataField
